e151ed36e7d200de8fd6b1677663b4e027568cf9
Vishal1003 <vishalj.ec18@nsut.ac.in>
2020-09-19 03:07:29 +0530

    algorithm worked!

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Node.css"
 
-export default function Node({isStart, isEnd, row, col}) {
+export default function Node({isStart, isEnd, row, col, isWall}) {
 
     // row and col are used to find the position of a certain box
 
-    const classes = isStart ? "node-start" : isEnd ? "node-end" : "";
+    const classes = isStart ? "node-start" : 
+                            isWall ? "node-wall" :
+                                isEnd ? "node-end" : "";
     return (
         <div className={`node ${classes}`} id={`node-${row}-${col}`} >
             
--- src/components/PathFind.js
+++ src/components/PathFind.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Node from './Node';
+import Astar from '../aStarAlgorithm/aStar'
 import "./PathFind.css";
 
-const cols = 25;
+const cols = 15;
 const rows = 10;
 
 
@@ -13,12 +14,15 @@ const NODE_END_COL = cols - 1;
 
 export default function PathFind() {
 
-    // to create and set the grids i.e basically the nodes
     const [Grid, setGrid] = useState([]);
+    const [Path, setPath] = useState([]);
+    const [VisitedNodes, setVisitedNodes] = useState([]);
 
     useEffect(() => {
         initiazeGrid();
-    },[]);
+    }, []);
+
+
 
     // SPOT constructor
     function Spot(i, j) {
@@ -34,10 +38,33 @@ export default function PathFind() {
         this.f = 0;
         this.h = 0;
 
-        
+        this.nbrs = [];
+        this.isWall = false;
+
+        if(Math.random(1) < 0.2){
+            this.isWall = true;
+        }
+
+        this.prev = undefined;
+        this.addNbrs = function (grid) {
+            let i = this.x;
+            let j = this.y;
+
+            if (i > 0)
+                this.nbrs.push(grid[i - 1][j]);
+            if (i < rows - 1)
+                this.nbrs.push(grid[i + 1][j]);
+
+            if (j > 0)
+                this.nbrs.push(grid[i][j - 1]);
+            if (j < cols - 1)
+                this.nbrs.push(grid[i][j + 1]);
+
+        }
+
     }
 
-    // creating a 2D array (better way may exist!)
+    // CREATING 2d GRID
     const initiazeGrid = () => {
         const grid = new Array(rows);
 
@@ -47,8 +74,29 @@ export default function PathFind() {
 
         createSpot(grid);
         setGrid(grid);
+        addNeighbor(grid);
+
+        const startNode = grid[NODE_START_ROW][NODE_START_COL];
+        const endNode = grid[NODE_END_ROW][NODE_END_COL];
+        let path = Astar(startNode, endNode);
+        startNode.isWall = false;
+        endNode.isWall = false;
+
+        setPath(path.path);
+        setVisitedNodes(path.visitedNodes);
     }
 
+
+    // ADD NBRS 
+    const addNeighbor = (grid) => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                grid[i][j].addNbrs(grid);
+            }
+        }
+    }
+
+    // CREATING SPOTS
     const createSpot = (grid) => {
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
@@ -65,8 +113,8 @@ export default function PathFind() {
                 return (
                     <div key={rowIndex} className="rowWrapper">
                         {row.map((col, colIndex) => {
-                            const { isStart, isEnd } = col;
-                            return <Node key={colIndex} isStart={isStart} isEnd={isEnd} row={rowIndex} col={colIndex}/>
+                            const { isStart, isEnd, isWall } = col;
+                            return <Node key={colIndex} isStart={isStart} isEnd={isEnd} row={rowIndex} col={colIndex} isWall={isWall} />
                         })}
                     </div>
                 )
@@ -74,8 +122,35 @@ export default function PathFind() {
         </div>
     )
 
+    const animateShortestPath = (shortestPathNodes) => {
+        for(let i=0; i<shortestPathNodes.length; i++){
+            setTimeout(() => {
+                const node = shortestPathNodes[i];
+                document.getElementById(`node-${node.x}-${node.y}`).className = "node node-shortest-path";
+            }, 10*i)
+        }
+    }
+
+    const visualizePath = () => {
+        for(let i=0; i<= VisitedNodes.length; i++){
+            if(i === VisitedNodes.length ){
+                setTimeout(() => {
+                    animateShortestPath(Path);
+                }, 20*i)
+            }
+            else {
+                setTimeout(() => {
+                    const node = VisitedNodes[i];
+                    document.getElementById(`node-${node.x}-${node.y}`).className = "node node-visited";
+                }, 10*i)
+            }
+        }
+    }
+
     return (
         <div className="wrapper">
+            <h1 style={{padding:"50px"}}>Path Finding Visualizer</h1>
+            <button onClick={visualizePath}>Visualize Path</button>
             {gridWithNodes}
         </div>
     )
